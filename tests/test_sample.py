83aa663ddbc9e404dd7a73a7ca07a7e9470381b6
Thales Maciel <thales@bode.io>
2021-01-04 04:57:32 -0300

    more tests :D

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,9 +1,17 @@
 import pytest
 
+
+def test_query_works_with_string(alfredo):
+    """Str is allowed in query"""
+    _, query = alfredo.parse_query('test')
+    assert 'test' == query
+
+
 def test_only_strings_allowed(alfredo):
     """Only str should be allowed in the query"""
     with pytest.raises(TypeError):
         alfredo.parse_query(12)
+
 
 def test_left_space_is_removed_from_query(alfredo):
     """Leading space should be ignored in the query"""
